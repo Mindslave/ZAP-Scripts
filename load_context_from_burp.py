d860cbb3e8fca70927311725484d7ebca658e455
Mindslave <mindslave.blog@protonmail.com>
2021-02-11 12:21:12 +0100

    finished the burp context import script

--- load_context_from_burp.py
+++ load_context_from_burp.py
@@ -1,5 +1,8 @@
 """ 
-
+This script will first prompt you to enter a name for the new context.
+Then you will be prompted to choose a file, here you should choose
+a burp project file (.json). The script will then create a new zap context
+that matches the "scope" defined in the burp suite project.
 """ 
 from javax.swing import JFrame;
 from javax.swing import JFileChooser;
@@ -30,8 +33,37 @@ def get_url_regexes(file_name):
 	includes = data['target']['scope']['include']
 	regexes = []
 	for include in includes:
-		regexes.append(include['host'])
+		host = get_host(include)
+		protocol = get_protocol(include)
+		if host.count(".") == 1:
+			proper_regex = add_www_case(protocol, host)
+			regexes.append(proper_regex)
+		proper_regex = build_proper_regex(protocol, host)
+		regexes.append(proper_regex)
 	return regexes
+
+def add_www_case(protocol, host):
+	host = "www." + host
+	proper_regex = protocol + host + ".*"
+	return proper_regex
+		
+
+def get_host(include):
+	host = str(include['host'])
+	host = host[1:-1]
+	return host
+
+def get_protocol(include):
+	protocol = str(include['protocol'])
+	if protocol == "http":
+		protocol = "http:\/\/"
+	else:
+		protocol = "https:\/\/"
+	return protocol
+
+def build_proper_regex(protocol, host):
+	proper_regex = protocol + host + ".*"
+	return proper_regex
 
 def create_new_context(ctx_name):
 	session = Model().getSingleton().getSession()
@@ -41,7 +73,7 @@ def create_new_context(ctx_name):
 def populate_context(url_regexes, context):
 	for pattern in url_regexes:
 		context.addIncludeInContextRegex(pattern)
-
+	
 ctx_name = get_context_name()
 file_name = get_file_name()
 ctx = create_new_context(ctx_name)
